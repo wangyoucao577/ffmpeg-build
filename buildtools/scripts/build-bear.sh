83b25014f38336dfeade5878f11c69e685696841
Jay Zhang <wangyoucao577@gmail.com>
2022-05-07 12:06:47 +0000

    feat: add bear to generate compile_commands.json

---
+++ buildtools/scripts/build-bear.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+echo "OSTYPE: $OSTYPE"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+    realpath() { # there's no realpath command on macosx 
+        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    }
+fi
+CURRENT_DIR_PATH=$(dirname $(realpath $0))
+PROJECT_ROOT_PATH=${CURRENT_DIR_PATH}/../../
+
+cd ${PROJECT_ROOT_PATH}/buildtools/bear
+
+# Prerequisites
+# apt-get install python cmake pkg-config
+# apt-get install libfmt-dev libspdlog-dev nlohmann-json3-dev \
+#                 libgrpc++-dev protobuf-compiler-grpc libssl-dev
+# See more details in https://github.com/rizsotto/Bear/blob/master/INSTALL.md
+
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_ROOT_PATH}/build -DENABLE_UNIT_TESTS=OFF -DENABLE_FUNC_TESTS=OFF ..
+make all
+make install
+cd ..
+
+cd ../..
--- buildtools/scripts/build-ffmpeg.sh
+++ buildtools/scripts/build-ffmpeg.sh
@@ -10,16 +10,22 @@ fi
 CURRENT_DIR_PATH=$(dirname $(realpath $0))
 PROJECT_ROOT_PATH=${CURRENT_DIR_PATH}/../../
 
-
 source ${CURRENT_DIR_PATH}/export-pkg-path.sh
-source ${CURRENT_DIR_PATH}/make-parallel-check.sh
+
+if [[ ${DISABLE_BEAR,,} = "true" || ${DISABLE_BEAR} = "1" ]]; then
+    source ${CURRENT_DIR_PATH}/make-parallel-check.sh
+else
+    BEAR_COMMAND="${PROJECT_ROOT_PATH}/build/bin/bear -- " 
+    MAKE_PARALLEL=""    # parallel make may fail due to bear
+fi
 
 # enter build foler
 cd ${PROJECT_ROOT_PATH}/ffmpeg
 
 # build ffmpeg, extra params will be appended at the end
 ./configure --prefix=${PROJECT_ROOT_PATH}/build --enable-gpl --enable-nonfree --enable-pic --enable-libsvtav1 "$@"
-make ${MAKE_PARALLEL} && make install
+${BEAR_COMMAND} make ${MAKE_PARALLEL} 
+make install
 
 # go back
 cd -
