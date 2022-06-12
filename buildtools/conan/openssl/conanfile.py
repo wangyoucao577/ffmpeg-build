053ce98500dc7a5bf8e6c781032c03e211f0557f
Jay Zhang <wangyoucao577@gmail.com>
2022-06-12 13:26:45 +0000

    Revert "refactor: remove useless path prefix"
    This reverts commit 3a91899b26c0a28543d1df1ca5692cbfd9f75fce.

--- buildtools/conan/openssl/conanfile.py
+++ buildtools/conan/openssl/conanfile.py
@@ -22,7 +22,9 @@ class OpensslConan(ConanFile):
                 "shared" if self.options.shared else "no-shared",
                 "no-tests"]
 
-        if self.settings.os == "Android":            
+        if self.settings.os == "Android":
+            path_prefix="PATH=${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}"
+
             # https://github.com/openssl/openssl/blob/master/NOTES-ANDROID.md
             args.append(" -D__ANDROID_API__=%s" % str(self.settings.os.api_level))  
             if self.settings.arch == "armv7":
@@ -38,7 +40,7 @@ class OpensslConan(ConanFile):
             # TODO: 
             pass
 
-        self.run("./Configure {args}".format(args=" ".join(args)))
+        self.run("{path_prefix} ./Configure {args}".format(path_prefix=path_prefix, args=" ".join(args)))
         self.run("make -j $(nproc)")
         self.run("make install")
 
