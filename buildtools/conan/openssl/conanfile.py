bbce7dc03ca4f7a69701e82f39c48e17dc8db06f
Jay Zhang <wangyoucao577@gmail.com>
2022-06-14 19:24:21 +0800

    feat: build openssl for ios

--- buildtools/conan/openssl/conanfile.py
+++ buildtools/conan/openssl/conanfile.py
@@ -37,11 +37,28 @@ class OpensslConan(ConanFile):
                 print("unknown android arch {}".format(self.settings.arch))
                 os.exit(1)
         elif self.settings.os == "iOS":
-            # TODO: 
-            pass
+            # https://github.com/openssl/openssl/blob/master/Configurations/15-ios.conf
+            # https://github.com/openssl/openssl/issues/15851
+            if self.settings.arch == "armv7":
+                args.append("ios-xcrun")
+                # args.append("-fembed-bitcode")
+
+                # Fixes "Undefined symbols for architecture armv7: ___atomic_is_lock_free"
+                # see more details in https://github.com/macports/macports-ports/blob/f543051794963064ea924697f7a33428936fbe2a/devel/openssl3/Portfile#L132-L136
+                args.append("-DBROKEN_CLANG_ATOMICS")
+            elif self.settings.arch == "armv8":
+                args.append("ios64-xcrun")
+                # args.append("-fembed-bitcode")
+            elif self.settings.arch == "x86_64":
+                args.append("iossimulator-xcrun")
+                args.append("-mios-simulator-version-min={}".format(self.settings.os.version))
+            else:
+                print("unknown ios arch {}".format(self.settings.arch))
+                os.exit(1)
+        args.append("-fvisibility=hidden")
 
         self.run("./Configure {args}".format(args=" ".join(args)))
-        self.run("make -j $(nproc)")
+        self.run("make -j {}".format(tools.cpu_count()))
         self.run("make install")
 
     def package_info(self):
