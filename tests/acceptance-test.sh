972340b29c4f6316208d0f5cb3b02011096a0836
Jay Zhang <wangyoucao577@gmail.com>
2023-01-07 17:21:19 +0800

    feat: add ffmpeg regression tests

--- tests/acceptance-test.sh
+++ tests/acceptance-test.sh
@@ -28,6 +28,6 @@ ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_264.mp4 -i ${TESTS_PATH}/small
 ${FFMPEG_BIN_PATH}/ffmpeg -y -threads 1 -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -c:v libx265 -c:a libfdk_aac ${TESTS_PATH}/out_hevc.mp4
 ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_hevc.mp4 -c copy ${TESTS_PATH}/out_hevc.flv
 ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_hevc.flv -c copy ${TESTS_PATH}/out_hevc2.mp4
-${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -vf drawtext="text='hello world':fontfile=${FFMPEG_BIN_PATH}/FreeSans.ttf" -f null -
+${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -vf drawtext="text='hello world':fontfile=${TESTS_PATH}/FreeSans.ttf" -f null -
 ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -vf subtitles=${TESTS_PATH}/sintel_en.srt -f null -
 ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -vn -af aresample=resampler=soxr -ar 8000 ${TESTS_PATH}/out.aac
