ed06f4da63650cb3c225f96a3e21854762cd845c
Jay Zhang <wangyoucao577@gmail.com>
2023-11-16 22:06:04 +0800

    feat: enable vmaf test on windows

--- tests/acceptance-test.bat
+++ tests/acceptance-test.bat
@@ -6,7 +6,7 @@ set TESTS_PATH=./tests
 "%FFMPEG_BIN_PATH%/ffmpeg" -y -c:v libdav1d -i "%TESTS_PATH%/out_av1.mp4" -f null -
 :: %FFMPEG_BIN_PATH%/ffmpeg -y -threads 1 -i %TESTS_PATH%/small_bunny_1080p_60fps.mp4 -c:v libaom-av1 -c:a copy %TESTS_PATH%/out.mp4
 "%FFMPEG_BIN_PATH%/ffmpeg" -y -threads 1 -i "%TESTS_PATH%/small_bunny_1080p_60fps.mp4" -c:v libx264 -c:a libopus "%TESTS_PATH%/out.mp4"
-:: %FFMPEG_BIN_PATH%/ffmpeg -y -i %TESTS_PATH%/out.mp4 -i %TESTS_PATH%/small_bunny_1080p_60fps.mp4 -lavfi "libvmaf=log_fmt=xml:log_path=/dev/stdout" -f null -
+"%FFMPEG_BIN_PATH%/ffmpeg" -y -i %TESTS_PATH%/out.mp4 -i %TESTS_PATH%/small_bunny_1080p_60fps.mp4 -lavfi "libvmaf" -f null -
 "%FFMPEG_BIN_PATH%/ffmpeg" -y -threads 1 -i "%TESTS_PATH%/small_bunny_1080p_60fps.mp4" -c:v libx265 -c:a libfdk_aac "%TESTS_PATH%/out_hevc.mp4"
 "%FFMPEG_BIN_PATH%/ffmpeg" -y -i "%TESTS_PATH%/out_hevc.mp4" -c copy "%TESTS_PATH%/out_hevc.flv"
 "%FFMPEG_BIN_PATH%/ffmpeg" -y -i "%TESTS_PATH%/out_hevc.flv" -c copy "%TESTS_PATH%/out_hevc2.mp4"
