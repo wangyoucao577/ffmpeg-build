03dc1fee3f848393e3bbd86252adf4c75c660a71
Jay Zhang <wangyoucao577@gmail.com>
2023-11-16 03:35:23 +0000

    fix: vmaf test by using built-in models

--- tests/acceptance-test.sh
+++ tests/acceptance-test.sh
@@ -24,7 +24,7 @@ ${FFMPEG_BIN_PATH}/ffmpeg -y -c:v libdav1d -i ${TESTS_PATH}/out_av1.mp4 -f null 
 # ${FFMPEG_BIN_PATH}/ffmpeg -y -threads 1 -i ./tests/small_bunny_1080p_60fps.mp4 -c:v libaom-av1 -c:a copy out.mp4
 
 ${FFMPEG_BIN_PATH}/ffmpeg -y -threads 1 -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -c:v libx264 -c:a libopus ${TESTS_PATH}/out_264.mp4
-${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_264.mp4 -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -lavfi "libvmaf=log_fmt=xml:log_path=/dev/stdout:model_path=${FFMPEG_BIN_PATH}/vmaf_v0.6.1.json" -f null -
+${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_264.mp4 -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -lavfi "libvmaf=log_fmt=xml:log_path=/dev/stdout" -f null -
 ${FFMPEG_BIN_PATH}/ffmpeg -y -threads 1 -i ${TESTS_PATH}/small_bunny_1080p_60fps.mp4 -c:v libx265 -c:a libfdk_aac ${TESTS_PATH}/out_hevc.mp4
 ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_hevc.mp4 -c copy ${TESTS_PATH}/out_hevc.flv
 ${FFMPEG_BIN_PATH}/ffmpeg -y -i ${TESTS_PATH}/out_hevc.flv -c copy ${TESTS_PATH}/out_hevc2.mp4
